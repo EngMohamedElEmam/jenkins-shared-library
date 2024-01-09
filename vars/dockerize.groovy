ed76e69f1e88bfb917d17702976e559040ba87c7
Mohamed ElEmam <Emam@MacBook-Pro.local>
2024-01-09 13:54:38 +0200

    Add My code

--- vars/dockerize.groovy
+++ vars/dockerize.groovy
@@ -2,16 +2,14 @@ def getCommitHash() {
     return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
 
-def buildDockerImage(String DOCKER_REGISTRY, String DOCKER_IMAGE) {
+def buildDockerImage(String DOCKER_REGISTRY, String DOCKER_IMAGE, String COMMIT_HASH) {
     script {
-        def COMMIT_HASH = getCommitHash()
         docker.build("${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${COMMIT_HASH}")
     }
 }
 
-def pushDockerImage(String DOCKER_REGISTRY, String DOCKER_IMAGE, String credentialsId) {
+def pushDockerImage(String DOCKER_REGISTRY, String DOCKER_IMAGE, String COMMIT_HASH, String credentialsId) {
     script {
-        def COMMIT_HASH = getCommitHash()
         withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_REGISTRY_USERNAME', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
             sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
             docker.image("${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${COMMIT_HASH}").push()
